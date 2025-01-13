dcc6f8a3691fe4ef5f73d0b78f1f006387bac432
Danny Regan <d.regan@columbia.edu>
2025-01-13 15:39:10 +0100

    API successfully accepts PDF inputs and adds them to uploads folder.

---
+++ backend/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify # Create and manage the web app, access incoming data from frontend, jsonify
+import os # interact with computer software (create folders)
+# import pdfplumber
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = 'uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
+def home():
+    return "Welcome to the Loan Assessor API by Danny Regan."
+
+@app.route('/parse-pdf', methods=['POST'])
+def parse_pdf():
+    if 'file' not in request.files:
+        return jsonify({"error": "No file provided"}), 400
+    
+    file = request.files['file']
+
+    if file.filename == '':
+        return jsonify({"error": "No selected file"}), 400
+
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+    file.save(file_path)
+
+    return "Success!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
